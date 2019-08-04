a12a8c04d01f4d4d79b576edecf28684db01522f
A.Alekseev <angelpsymail@gmail.com>
2019-08-04 23:06:23 +0500

    add deploy to gh-pages

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
     lintOnSave: false,
     css: {
         sourceMap: true
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/markup/'
+        : '/'
 }
